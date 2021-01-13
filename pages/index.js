d43289b82d4c382118c74dabeb012034a54cb062
Isaac Pontes <isaaclfp@outlook.com>
2021-01-12 23:15:40 -0300

    Corrected className attributes

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,26 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main className={styles.main}>
-        <h1 class="title">
+        <h1 className="title">
           Bulma
         </h1>
 
-        <p class="subtitle">
+        <p className="subtitle">
           Modern CSS framework based on <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox">Flexbox</a>
         </p>
 
-        <div class="field">
-          <div class="control">
-            <input class="input" type="text" placeholder="Input" />
+        <div className="field">
+          <div className="control">
+            <input className="input" type="text" placeholder="Input" />
           </div>
         </div>
 
-        <div class="field">
-          <p class="control">
-            <span class="select">
+        <div className="field">
+          <p className="control">
+            <span className="select">
               <select>
                 <option>Select dropdown</option>
               </select>
@@ -34,22 +28,11 @@ export default function Home() {
           </p>
         </div>
 
-        <div class="buttons">
-          <a class="button is-primary">Primary</a>
-          <a class="button is-link">Link</a>
+        <div className="buttons">
+          <a className="button is-primary">Primary</a>
+          <a className="button is-link">Link</a>
         </div>        
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
